47a6e3ba8fd03f1f98e81d71cea1b2123e7b44b4
Paulina Pavičić <ppavicic@racunarstvo.hr>
2025-04-13 20:51:37 +0200

    Created Project 2

---
+++ Project 2/FileChoiceWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_2
+{
+    /// <summary>
+    /// Interaction logic for FileChoiceWindow.xaml
+    /// </summary>
+    public partial class FileChoiceWindow : Window
+    {
+        public FileChoiceWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
